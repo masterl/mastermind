058271bf2d3cc8540a9b98637681ee33a74b4d20
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-06-08 14:10:59 -0700

    Adds MasterMind class

---
+++ src/MasterMind.cpp
@@ -0,0 +1,43 @@
+#include "MasterMind.hpp"
+
+std::string MasterMind::checkGuess( std::string answer, std::string guess )
+{
+    std::string result{"    "};
+    int i = 0;
+    char const MATCHED = ' ';
+
+    for( unsigned k = 0; k < answer.length(); ++k )
+    {
+        if( answer[k] == guess[k] )
+        {
+            result[i] = '*';
+            ++i;
+
+            answer[k] = MATCHED;
+        }
+    }
+
+    for( unsigned k = 0; k < answer.length(); ++k )
+    {
+        if( answer[k] == MATCHED )
+        {
+            continue;
+        }
+
+        for( unsigned m = 0; m < guess.length(); ++m )
+        {
+            if( guess[m] == MATCHED )
+            {
+                continue;
+            }
+
+            if( answer[k] == guess[k] )
+            {
+                result[i] = '-';
+                ++i;
+            }
+        }
+    }
+
+    return result;
+}
---
+++ src/MasterMind.hpp
@@ -0,0 +1,12 @@
+#ifndef MASTERMIND_H
+#define MASTERMIND_H
+
+#include <string>
+
+class MasterMind
+{
+public:
+    std::string checkGuess( std::string answer, std::string guess );
+};
+
+#endif
