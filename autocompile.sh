e8b00911f81be0d3d4b92d1eff7b6431f77601c4
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-06-08 15:13:39 -0700

    Removes old files

---
+++ autocompile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+readonly PROJECT_ROOT=$( cd "$( dirname "$0" )" && pwd )
+readonly FILE_PATH="$1"
+
+readonly FILE_DIR=$(dirname "$FILE_PATH")
+
+readonly EXECUTABLE="$FILE_DIR/program.bin"
+
+readonly COMPILE_COMMAND="g++ -Wall -std=c++14 $FILE_PATH -o $EXECUTABLE"
+
+readonly GIT_STATUS_COMMAND="git status"
+readonly PRINT_LINE="echo \\\"==================================================\\\""
+
+entr_command="entr -d sh -c \""
+entr_command="$entr_command tput reset;"
+entr_command="$entr_command echo \\\"Compiling...\\\";"
+entr_command="$entr_command $PRINT_LINE;"
+entr_command="$entr_command $COMPILE_COMMAND;"
+entr_command="$entr_command echo; $PRINT_LINE;"
+entr_command="$entr_command echo \\\"Running GIT Status...\\\";"
+entr_command="$entr_command $PRINT_LINE;"
+entr_command="$entr_command $GIT_STATUS_COMMAND;"
+entr_command="$entr_command echo;"
+entr_command="$entr_command date;"
+entr_command="$entr_command\""
+
+readonly FULL_COMMAND_STRING="echo $FILE_PATH | $entr_command"
+
+while true; do
+  eval "$FULL_COMMAND_STRING"
+done
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-#include <string>
 #include <cctype>
-#include <vector>
+#include <chrono>
 #include <fstream>
+#include <iostream>
 #include <random>
+#include <string>
+#include <vector>
 
 using namespace std;
 namespace utils
@@ -11,7 +12,7 @@ namespace utils
     namespace files
     {
         typedef std::ifstream::streampos fsize_t;
-        
+
         fsize_t filesize( std::string const &filename );
     } // namespace files
 } // namespace utils
@@ -23,31 +24,31 @@ namespace utils
         fsize_t filesize( std::string const &filename )
         {
             std::ifstream file( filename.c_str(),
-                               std::ifstream::ate | std::ifstream::binary );
+                                std::ifstream::ate | std::ifstream::binary );
             return file.tellg();
         }
     } // namespace files
-} // namespac
+} // namespace utils
 
 class RandomIntGenerator
 {
 public:
     RandomIntGenerator( void );
     int operator()( int const min = 0, int const max = 10 );
-    
+
 private:
     std::ranlux24 generator_;
 };
 
 RandomIntGenerator::RandomIntGenerator( void )
-: generator_( std::chrono::system_clock::now().time_since_epoch().count() )
+    : generator_( std::chrono::system_clock::now().time_since_epoch().count() )
 {
 }
 
 int RandomIntGenerator::operator()( int const min, int const max )
 {
     std::uniform_int_distribution< int > distribution( min, max );
-    
+
     return distribution( generator_ );
 }
 
@@ -55,17 +56,17 @@ class WordBank
 {
 public:
     typedef unsigned long long bank_size_t;
-    
+
     WordBank( bank_size_t const bank_size = 0 );
-    
+
     bool load_from_file( std::string const &filename,
-                        unsigned const word_length );
+                         unsigned const word_length );
     void add_word( std::string word );
-    
+
     std::string random();
-    
+
     std::string normalizeWord( std::string word );
-    
+
 private:
     std::vector< std::string > words_;
     RandomIntGenerator random_;
@@ -73,39 +74,39 @@ private:
 };
 
 WordBank::WordBank( bank_size_t bank_size )
-: words_{bank_size}
+    : words_{bank_size}
 {
 }
 
 bool WordBank::load_from_file( std::string const &filename,
-                              unsigned const word_length )
+                               unsigned const word_length )
 {
     namespace ufiles = utils::files;
-    
+
     std::streampos const file_size = ufiles::filesize( filename );
     unsigned long long const words_count = file_size / word_length;
-    
+
     this->word_length_ = word_length;
-    
+
     words_.clear();
     words_.reserve( words_count );
-    
+
     std::ifstream file{filename};
-    
+
     if( !file.is_open() )
     {
         return false;
     }
-    
+
     for( unsigned long long i = 0; i < words_count; ++i )
     {
         std::string word;
-        
+
         file >> word;
-        
+
         this->add_word( word );
     }
-    
+
     return true;
 }
 
@@ -115,7 +116,7 @@ void WordBank::add_word( std::string word )
     {
         return;
     }
-    
+
     words_.emplace_back( normalizeWord( word ) );
 }
 
@@ -125,9 +126,9 @@ std::string WordBank::random()
     {
         return "";
     }
-    
+
     int const index = random_( 0, words_.size() - 1 );
-    
+
     return words_[index];
 }
 
@@ -137,7 +138,7 @@ std::string WordBank::normalizeWord( std::string word )
     {
         letter = tolower( letter );
     }
-    
+
     return word;
 }
 
@@ -146,7 +147,7 @@ class MasterMind
 public:
     static std::string const RIGHT_GUESS;
     static std::string const WRONG_GUESS;
-    
+
     std::string checkGuess( std::string answer, std::string guess );
 };
 std::string const MasterMind::RIGHT_GUESS{"****"};
@@ -157,92 +158,97 @@ std::string MasterMind::checkGuess( std::string answer, std::string guess )
     std::string result{"    "};
     int i = 0;
     char const MATCHED = ' ';
-    
+
     for( unsigned k = 0; k < answer.length(); ++k )
     {
         if( answer[k] == guess[k] )
         {
             result[i] = '*';
             ++i;
-            
+
             answer[k] = MATCHED;
+            guess[k] = MATCHED;
         }
     }
-    
+
     for( unsigned k = 0; k < answer.length(); ++k )
     {
         if( answer[k] == MATCHED )
         {
             continue;
         }
-        
+
         for( unsigned m = 0; m < guess.length(); ++m )
         {
             if( guess[m] == MATCHED )
             {
                 continue;
             }
-            
-            if( answer[k] == guess[k] )
+
+            if( answer[k] == guess[m] )
             {
                 result[i] = '-';
                 ++i;
             }
         }
     }
-    
+
     return result;
 }
 
-string getUserInput() {
+string getUserInput()
+{
     string userInputStr;
-    getline(cin, userInputStr);
+    getline( cin, userInputStr );
     return userInputStr;
 }
 
-void display(int count, string guessResult) {
-    if(guessResult == "****")
+void display( int count, string guessResult )
+{
+    if( guessResult == "****" )
         cout << "Computer: You got it! " << count << " tries." << endl;
-    else if(guessResult == "    ")
+    else if( guessResult == "    " )
         cout << "Computer: You got nothing. Sorry." << endl;
     else
         cout << guessResult << endl;
 }
 
-string toLowerCase(string str) {
-    for(int i = 0; i < str.length(); i++) {
-        str[i] = tolower(str[i]);
+string toLowerCase( string str )
+{
+    for( int i = 0; i < str.length(); i++ )
+    {
+        str[i] = tolower( str[i] );
     }
     return str;
 }
 
-int main() {
+int main()
+{
     int guessCount = 0;
     string userInput;
     string answer;
     string guess;
-    
+
     MasterMind masterMind;
     WordBank wordBank;
-    wordBank.load_from_file("4_letter_words.txt", 4);
+    wordBank.load_from_file( "4_letter_words.txt", 4 );
     answer = wordBank.random();
     cout << "Computer: I have a 4 letter word in mind. Can you guess it?"
-    << endl;
-    
-    while(true) {
+         << endl;
+
+    while( true )
+    {
         cout << "Answer: " << answer << endl;
         cout << "Player: ";
         userInput = getUserInput();
-        userInput = toLowerCase(userInput);
-        guess = masterMind.checkGuess(answer, userInput);
-        display(guessCount, guess);
-        if(guess == "****")
+        userInput = toLowerCase( userInput );
+        guessCount++;
+        guess = masterMind.checkGuess( answer, userInput );
+        cout << "Result: [" << guess << "]" << endl;
+        display( guessCount, guess );
+        if( guess == "****" )
             break;
     }
-    
-    
-    
 
-    
     return 0;
 }
